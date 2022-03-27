2c7b9a731adacdc716bb471bb2de0f593ef4dc9f
P1k@b00h <nicolas.lefaure@gmail.com>
2022-03-27 18:12:15 +0200

    finalisation du snke, gestion collision bordure et coprs du serpent. Mager la pomme, croissance du serpent, début mise en fomre du jeu

--- SnakeProject/script.js
+++ SnakeProject/script.js
@@ -7,12 +7,11 @@ window.onload = function () {
     // et celle de nos blocks
     let blockSize = 30;
 
-
     // variable pour créer l'élement canvas sur le html
     let canvas = document.createElement('canvas');
     //pour définir le ctx de notre canvas
     let ctx;
-    // delais pour lancer les animations (en ms)
+    // delais pour lancer les animations (en ms), servira à définir la vitesse du jeu (du serpent)
     let delay = 100;
 
     // soit les coordonées en x & y de notre serpent
@@ -24,9 +23,10 @@ window.onload = function () {
     let apple;
 
     //ceci va nous permettre de calculer le nombre de block dans la largeur et la hauteur (20*20)
-    let widthInBlocks = canvasWidth/blockSize;
-    let heightInBlocks = canvasHeight/blockSize;
+    let widthInBlocks = canvasWidth / blockSize;
+    let heightInBlocks = canvasHeight / blockSize;
 
+    let score;
 
     // lancement de la fonction init permettant l'initialisation
     init();
@@ -38,36 +38,112 @@ window.onload = function () {
         // définition de notre canvas et affichage ne html
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
-        canvas.style.border = '1px solid';
+        canvas.style.border = '30px solid grey';
+
+        canvas.style.margin = "50px auto";
+        canvas.style.display = "block";
+        canvas.style.backgroundColor = "#ddd";
+
         document.body.appendChild(canvas);
         //defini le context du canvas en 2d
         ctx = canvas.getContext('2d');
 
         // création du serpent (position, taille, et direction de déplacement initial)
-        snake = new Snake([[6, 4], [5, 4], [4, 4]], 'right')
-
-        apple = new Apple([10,10]);
-
+        snake = new Snake([[6, 4], [5, 4], [4, 4]/*,[3,4], [2,4], [1,4]*/], 'right')
+        apple = new Apple([10, 10]);
+        score = 0;
         refreshCanvas();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //donnera l'impression que les élements bougent
+    // Et définira les éléments de jeux (collisions, mouvement, manger la pomme, GAME OVER ...)
     function refreshCanvas() {
 
         //xCoord += 10;
         //yCoord += 10;
-
-        // à chaque appel de notre fonction nous allons "effacer" notre serpent
-        ctx.clearRect(0, 0, canvas.height, canvas.height);
-
+        // appel de notre fonction pour faire avancer le serpent
         snake.advance();
-        // appel de la method draw de la class Snake (instancié dans snake)
-        snake.draw();
-        // nous allons dessiner la pomme
-        apple.draw();
-        // nous permettra de relancer la fonction (ici refreshCanvas) tous les temps donnés (ici delay soit 1000 ms)
-        setTimeout(refreshCanvas, delay);
+        // à partir du moment ou le serpent se déplace on doit controler les collisions potentielles
+        if (snake.checkCollision()) {
+            // GAME OVER
+            gameOver();
+        } else {
+            if (snake.isAppleEat(apple)) {
+                //le serpent à mangé la pomme
+                // On augmente le score de 1 point
+                score++;
+                //puis création d'une nouvelle pomme
+                // tant que la nouvelle pomme se trouve à un endroit ou il y a le serpent on relance une création de pomme
+                snake.ateApple = true;
+                do {
+                    apple.setNewApplePosition()
+                }
+                while (apple.isAppleOnSnake(snake))
+            }
+            // à chaque appel de notre fonction nous allons "effacer" notre serpent
+            ctx.clearRect(0, 0, canvas.height, canvas.height);
+            // affichage du score sur le canvas en fond (l'ordre d'appel déterminera leurs 'hauteur')
+            displayScore();
+            // nous allons dessiner la pomme
+            apple.draw();
+            // appel de la method draw de la class Snake (instancié dans snake)
+            snake.draw();
+            // nous permettra de relancer la fonction (ici refreshCanvas) tous les temps donnés (ici delay soit 1000 ms)
+            setTimeout(refreshCanvas, delay);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Gestion du game Over avec la fonction gameOver ...
+    function gameOver()
+    {
+        ctx.save();
+        ctx.font = "bold 60px sans-serif";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 5;
+
+        ctx.strokeText('GAME OVER DUDE !', canvasWidth/2, canvasHeight/2 );
+        // filltext nous permttra d'afficher un text lors d'un cas de gameOver
+        ctx.fillText('GAME OVER DUDE !', canvasWidth/2, canvasHeight/2);
+
+        ctx.strokeText("Score final : " + score.toString(), canvasWidth/2, canvasHeight/2 - 100)
+        ctx.fillText("Score final : " + score.toString(), canvasWidth/2, canvasHeight/2 - 100);
+
+        ctx.font = "bold 25px sans-serif";
+        ctx.strokeText("Relancer une partie avec la touche Espace" , canvasWidth/2, canvasHeight/2 + 150)
+        ctx.fillText("Relancer une partie avec la touche Espace" , canvasWidth/2, canvasHeight/2 + 150)
+
+
+        ctx.restore();
+    };
+
+    function restartGame()
+    {
+        // On recréé le serpent (position, taille, et direction de déplacement initial)
+        snake = new Snake([[6, 4], [5, 4], [4, 4]/*,[3,4], [2,4], [1,4]*/], 'right')
+        // ainsi que la pomme
+        apple = new Apple([10, 10]);
+        score = 0;
+        refreshCanvas();
+    };
+
+    function displayScore()
+    {
+        ctx.save();
+        // Stylisons notre résultat
+        ctx.font = "bold 200px sans-serif";
+        ctx.fillStyle = "gray";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+
+        // filltext nous permettra d'afficher un text lors d'un cas de gameOver
+        ctx.fillText(score.toString(), canvasHeight/2, canvasWidth/2, 100);
+
+        ctx.restore();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -90,6 +166,7 @@ window.onload = function () {
     function Snake(body, direction) {
         this.body = body;
         this.direction = direction;
+        this.ateApple = false;
         this.draw = function () {
             // va sauver notre canvas
             ctx.save();
@@ -132,15 +209,21 @@ window.onload = function () {
                 default:
                     throw("Direction invalide");
             }
-
             // on va ajouter notre nextPosition (la 'nouvelle' tête)
             this.body.unshift(nextPosition);
-            // supprimer la derniere positon (la queue du serpent)
-            this.body.pop();
+            // Si le serpent n'a pas mangé de pomme alors
+            if (!this.ateApple) {
+                // supprimer la derniere positon (la queue du serpent)
+                this.body.pop();
+            } else {
+                // sinon on ne fait pas de pop() sur le body
+                // le serpent va "grandir"
+                // afin d'arréter sa "croissance" on repasse le ateApple à false => retour à l'état initial
+                this.ateApple = false;
+            }
         };
 
-        this.setDirection = function(newDirection)
-        {
+        this.setDirection = function (newDirection) {
             let allowedDirection;
             // on régle les possibilitées de déplacement en fonciton du déplacement en cours
             switch (this.direction) {
@@ -159,17 +242,17 @@ window.onload = function () {
 
             // si l'index de ma nouvelle direction est supérieur à -1 (newDirection est une array avec 2 valeurs soit 0 soit 1) alors on défini la nouvelle direction,
             // si la valeur n'est pas présente dans l'array alors le résulat sera égal à -1 donc le déplacement ne sera pas permis (impossible au serpent de revenir sur lui même)
-            if(allowedDirection.indexOf(newDirection) > -1)
-            {
+            if (allowedDirection.indexOf(newDirection) > -1) {
                 this.direction = newDirection;
             }
         };
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // method pour les test de collision
         // pour rappel les bords du canvas et le corps du seprent lui-même
-        this.checkCollision = function ()
-        {
+        this.checkCollision = function () {
+            // gestion de collision avec mur false par défaut
             let wallCollision = false;
+            // gestion de collision avec corps (seprent) en false apr défaut également
             let snakeCollision = false;
 
             // on sépare la tête du serpent, c'est sur elle que l'on doit vérifier la collision
@@ -178,19 +261,51 @@ window.onload = function () {
             let snakeBody = this.body.slice(1);
             // on récupére les coordonnées de la tête en X & Y
             // le snakeX devra être compris entre 0 & 19 => widthInBlocks (soit 20 blocks)
-            let snakeX = snakeHead[0];
+            let snakeHeadX = snakeHead[0];
             // le snakeY devra être compris entre 0 & 19 => widthInBlocks (soit 20 blocks)
-            let snakeY = snakeHead[1];
+            let snakeHeadY = snakeHead[1];
 
             // on défini donc nos limites
-            let minX = 0;
-            let minY = 0;
-            let maxX = widthInBlocks - 1;
-            let maxY = heightInBlocks - 1;
+            let minX = 0; // début axe x
+            let minY = 0; // début axe y
+            // soit widthInBlocks la largeur diviser par la taille d'un block, cela nous permets de diviser notre canvas en block (20*20)
+            let maxX = widthInBlocks - 1; // fin axe x
+            // heightInBlocks => hautreur en nbr de blocks (20*20)
+            let maxY = heightInBlocks - 1; // fin axe y
 
-            let isNotBetweenWidth = snakeX < minX || snakeX > maxX;
-            let isNotBetweenHeight = snakeY < minY || snakeY > maxY;
+            //Donc notre snakeHeadX doit être compris en 0 et 19 (nos limites de blocks)
+            let isNotBetweenWidth = snakeHeadX < minX || snakeHeadX > maxX;
+            // idem pour le snakeHeadY qui lui aussi doit être compris entre 0 et 19 (sur l'axe y)
+            let isNotBetweenHeight = snakeHeadY < minY || snakeHeadY > maxY;
+            // Si l'on dépasse l'une ou l'autre des limites définis alors wallCollision passera à true
+            if (isNotBetweenWidth || isNotBetweenHeight) {
+                wallCollision = true;
+            }
+
+            // Gérons maintenant les collisions avec le corps du serpent lui même
+            // Pour que la collision soit complête il faut que le X ET le Y de la tête soit égale avec l'un des éléments du corps
+            for (let i = 0; i < snakeBody.length; i++) {
+                // rappel nous avons une array d'array, la premiére (avec le i) représente la global (snakeBody) la seconde représente les axes X [0] et y [1]
+                if (snakeBody[i][0] === snakeHeadX && snakeBody[i][1] === snakeHeadY)
+                    snakeCollision = true;
+            }
+            // la fonction checkCollision nous fera donc un retour sur wallCollision et snakeCollision, dans le cas ou l'un OU l'autre passe à true alors GAME OVER
+            return wallCollision || snakeCollision;
         };
+
+        // La pomme a-t-elle était mangée ?
+        // cette method sera appelée dans notre refreshCanvas
+        this.isAppleEat = function (appleToEat) {
+            //nous définissons la tête de notre serpent (déjà défini une premiere fois dans la methode checkCollision
+            let snakeHead = this.body[0];
+            // comme pour les collisions la pomme ne sera mangé que si la t^te en X & en Y est égale avec la posiotn de la pomme en X & en Y
+            if (snakeHead[0] === appleToEat.position[0] && snakeHead[1] === appleToEat.position[1]) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -199,31 +314,51 @@ window.onload = function () {
     function Apple(position) {
         this.position = position;
 
-
-        this.draw = function ()
-        {
+        this.draw = function () {
             //on sauvegarde la valeur précédente du canvas
             ctx.save();
             ctx.fillStyle = "#33cc33";
-
+            //afin d'éviter que la posiotn de la pomme ne soit conservée après avoir était mangé par le serpent on fait un beginPath() =>
+            // method of the Canvas 2D API starts a new path by emptying the list of sub-paths. Call this method when you want to create a new path.
+            ctx.beginPath();
             // soit radius le rayon de la pomme
-            let radius = blockSize/2;
+            let radius = blockSize / 2;
             // Pour dessiner un cercle nous voulons les coordonnées x&y mais la ou pour le rectangle nous voulions les deux coins du carré ici nous voulons le centre du cercle
-            let x = position[0]*blockSize + radius;
-            let y = position[1]*blockSize + radius;
+            let x = this.position[0] * blockSize + radius;
+            let y = this.position[1] * blockSize + radius;
             //le calcul pour dessiner le cercle et le suivant =>
-            ctx.arc(x, y, radius, 0, Math.PI*2, true);
+            ctx.arc(x, y, radius, 0, Math.PI * 2, true);
             // puis comme pour le serpent on rempli le tracé
             ctx.fill();
             // on restaure le canvas ceci permettra d'éviter "d'écraser" la couleur du serpent par du vert (entre autre)
             ctx.restore();
-        }
+        };
+        // Dans le cas ou la pomme est mangé il faut en créer une nouvelle, cette méthode sera appelé dans notre fonction refreshCanvas
+        this.setNewApplePosition = function () {
+            // la nouvelle position devrait être généré de manière aléatoire
+            // on veut un résultat arrondie : Math.round(), et généré aléatoirement : Math.random.
+            // Ce nombre aléatoire dot être contenu en 0 et 19, or random donne un résultat entre 0 et 1, alors on multiplie ce résutat par le nombre de block (-1, soit de 0 à 19)
+            let newX = Math.round(Math.random() * (widthInBlocks - 1));
+            let newY = Math.round(Math.random() * (heightInBlocks - 1));
+            this.position = [newX, newY];
+        };
+        // La pomme aura donc une nouvelle position, le problème c'est que cette position peut être égale à une zone du serpent, nous ne voulons pas de cette éventualitée :
+        // ici encore cette method sera appelé dans le refreshCanvas
+        this.isAppleOnSnake = function (snakeToCheck) {
+            let isAppleOnSnake = false;
+
+            for (let i = 0; i < snakeToCheck.body.length; i++) {
+                if (snakeToCheck.body[i][0] === this.position[0] && snakeToCheck.body[i][1] === this.position[1]) // on vérifie qu'aucune partire du serpent (à la fois en X & Y) n'est sur la pomme
+                {
+                    isAppleOnSnake = true;
+                }
+                ;
+            }
+            return isAppleOnSnake; // on retourne donc un booléen indiquant si la nouvelle pomme se trouve dans le corps du serpent
+        };
     }
 
-
-
-
-        document.addEventListener("keydown", function handleKeyDown(e) {
+    document.addEventListener("keydown", function handleKeyDown(e) {
         let key = e.code;
         let newDirection;
         switch (key) {
@@ -243,7 +378,13 @@ window.onload = function () {
             case 'KeyS':
                 newDirection = 'down';
                 break;
-                // si une autre touche est appuyer alors on return la fonction
+
+            case '32':
+            case 'Space':
+                restartGame();
+                return;
+
+            // si une autre touche est appuyé alors on return la fonction
             default:
                 return;
         }
